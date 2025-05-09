6ba9eec7ec5ad1348f3437e2de5414069868d91f
junyeong <whwnsdud4592@naver.com>
2025-05-09 21:58:24 +0900

    feat : project load

---
+++ src/test/java/my_slipp/slipp/SlippApplicationTests.java
@@ -0,0 +1,13 @@
+package my_slipp.slipp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlippApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
